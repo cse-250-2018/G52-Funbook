99f63bca78dfdb9217e9f07ac583fd877ba6e315
Preity <samiapreity23dnt@gmail.com>
2021-11-30 12:55:12 +0600

    Add First Page

--- app/src/main/java/com/preity/lalala/MainActivity.java
+++ app/src/main/java/com/preity/lalala/MainActivity.java
@@ -2,13 +2,37 @@ package com.preity.lalala;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.preity.lalala.R;
+import com.preity.lalala.firstPage;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_1;
+    private Button btn_2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_1 =  findViewById(R.id.btn_1);
+        btn_2 =  findViewById(R.id.btn_2);
+
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenFirstPage();
+            }
+        });
+
+    }
+
+    public void OpenFirstPage() {
+        Intent intent= new Intent(this, firstPage.class );
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/preity/lalala/firstPage.java
@@ -0,0 +1,47 @@
+package com.preity.lalala;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class firstPage extends AppCompatActivity {
+
+    Button btn;
+    ImageView img;
+    int SELECT_PICTURE = 200;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagepicker();
+            }
+        });
+    }
+    void imagepicker() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+
+        }
+    }
+}
